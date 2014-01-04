9ee6cde71a1737609f2b3be62aa941ac12179996
Shimon Xin <shimon.xin@gmail.com>
2014-01-04 18:07:42 +0800

    exclude log4j.properties

--- lms/lms-spi/src/main/java/com/github/shimonxin/lms/spi/messaging/impl/LmaxQueueMessaging.java
+++ lms/lms-spi/src/main/java/com/github/shimonxin/lms/spi/messaging/impl/LmaxQueueMessaging.java
@@ -2,13 +2,13 @@ package com.github.shimonxin.lms.spi.messaging.impl;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.shimonxin.lms.proto.AbstractMessage;
 import com.github.shimonxin.lms.proto.ConnectMessage;
 import com.github.shimonxin.lms.proto.DisconnectMessage;
-import com.github.shimonxin.lms.proto.PingReqMessage;
 import com.github.shimonxin.lms.proto.PubAckMessage;
 import com.github.shimonxin.lms.proto.PubCompMessage;
 import com.github.shimonxin.lms.proto.PubRecMessage;
